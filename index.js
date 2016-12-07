67e5287aa122d8ca3f4a84a54eaaf9dddde4a4f0
Jared Hanson <jaredhanson@gmail.com>
2016-12-07 12:28:22 -0800

    Duo Web implementation.

---
+++ index.js
@@ -0,0 +1,11 @@
+exports = module.exports = {
+};
+
+exports.load = function(id) {
+  try {
+    return require('./xom/' + id);
+  } catch (ex) {
+    if (ex.code == 'MODULE_NOT_FOUND') { return; }
+    throw ex;
+  }
+};
