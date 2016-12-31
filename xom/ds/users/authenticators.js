68927cdeaa20576fa3c7c6ce453cb4e85e870115
Jared Hanson <jaredhanson@gmail.com>
2016-12-31 12:34:05 -0800

    Consolidate authenticators directory.

--- xom/ds/users/authenticators.js
+++ xom/ds/users/authenticators.js
@@ -1,8 +1,56 @@
-exports = module.exports = function(idmap, client) {
-  // Load modules.
-  var Directory = require('../../../lib/creds/directory');
+var merge = require('utils-merge');
+
+
+function UserAuthenticatorsDirectory(client, idmap) {
+  this._client = client;
+  this._idmap = idmap;
+}
+
+UserAuthenticatorsDirectory.prototype.list = function(user, options, cb) {
+  if (typeof options == 'function') {
+    cb = options;
+    options = undefined;
+  }
+  options = options || {};
   
-  var directory = new Directory(client, idmap);
+  var self = this;
+  this._idmap(user, function(err, idparams) {
+    if (err) { return cb(err); }
+    
+    var params = {};
+    merge(params, idparams);
+    
+    self._client.jsonApiCall('POST', '/auth/v2/preauth', params, function(data) {
+      if (data.stat !== 'OK') {
+        return cb(new Error('TODO: something went wrong'));
+      }
+    
+      var devices = data.response.devices, device;
+      var credentials = [], credential;
+      var i, len;
+    
+      for (i = 0, len = devices.length; i < len; ++i) {
+        device = devices[i];
+        credential = {};
+        credential.id = device.device;
+        credential.methods = [];
+        if (device.capabilities.indexOf('mobile_otp') !== -1) {
+          credential.methods.push('otp');
+        }
+        
+        credentials.push(credential);
+      }
+      
+      return cb(null, credentials);
+    });
+  });
+};
+
+
+
+
+exports = module.exports = function(idmap, client) {
+  var directory = new UserAuthenticatorsDirectory(client, idmap);
   return directory;
 };
 
@@ -12,6 +60,6 @@ exports['@implements'] = [
 ];
 exports['@singleton'] = true;
 exports['@require'] = [
-  '../id/map',
+  '../../id/map',
   'http://schemas.authnomicon.org/js/login/mfa/opt/duo/Client'
 ];
