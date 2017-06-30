e6c6675a0057a9a49038e72cbb9f549ea6e08005
Jared Hanson <jaredhanson@gmail.com>
2017-06-30 13:13:09 -0700

    Confirm test case.

--- test/confirm.test.js
+++ test/confirm.test.js
@@ -17,6 +17,45 @@ describe('confirm', function() {
     };
     
     
+    describe('confirmed', function() {
+      var params;
+      
+      before(function() {
+        var result = { response: 'success', stat: 'OK' };
+        
+        sinon.stub(client, 'jsonApiCall').yields(result);
+      });
+    
+      after(function() {
+        client.jsonApiCall.restore();
+      });
+      
+      before(function(done) {
+        var confirm = factory(client);
+        
+        confirm({ userID: 'XXXXX00XXXXXX00XXX0X', code: 'duo://0x0xXx0XXXXxxXXX00xX-XXXxXXXxXxXxXXX0XxX0x0XxX0XxxXX0XxXxxX' }, function(_err, _params) {
+          if (_err) { return done(_err); }
+          params = _params;
+          done();
+        });
+      });
+    
+      it('should perform authentication via Auth API', function() {
+        expect(client.jsonApiCall).to.have.been.calledOnce;
+        var call = client.jsonApiCall.getCall(0);
+        expect(call.args[0]).to.equal('POST');
+        expect(call.args[1]).to.equal('/auth/v2/enroll_status');
+        expect(call.args[2]).to.deep.equal({
+          user_id: 'XXXXX00XXXXXX00XXX0X',
+          activation_code: 'duo://0x0xXx0XXXXxxXXX00xX-XXXxXXXxXxXxXXX0XxX0x0XxX0XxxXX0XxXxxX'
+        });
+      });
+      
+      it('should yield parameters', function() {
+        expect(params).to.equal(true);
+      });
+    }); // confirmed
+    
     describe('a typical authenticator', function() {
       var params;
       
@@ -52,7 +91,7 @@ describe('confirm', function() {
       });
       
       it('should yield parameters', function() {
-        expect(params).to.equal(true);
+        expect(params).to.equal(false);
       });
     }); // a typical authenticator
     
