b8011c21faf0420862b92f056d791d56f0ef2b72
Jared Hanson <jaredhanson@gmail.com>
2017-06-30 11:25:23 -0700

    Add test case for listing enroll.

--- test/ds/users/authenticators.test.js
+++ test/ds/users/authenticators.test.js
@@ -99,6 +99,61 @@ describe('duo/ds/credentials', function() {
         
       }); // user with app installed on iPhone
       
+      describe('user that needs to enroll', function() {
+        var credentials;
+        
+        before(function() {
+          var record = {
+            response: {
+              enroll_portal_url: 'https://api-x0xxxx00.duosecurity.com/portal?code=x00x0x0xx0xx00x0&akey=XXXXX0XXX0XXXX0XXXX0',
+              result: 'enroll',
+              status_msg: 'Enroll an authentication device to proceed'
+            },
+            stat: 'OK'
+          };
+          
+          sinon.stub(client, 'jsonApiCall').yields(record);
+          idmap = sinon.stub().yields(null, { username: 'johndoe' });
+        });
+      
+        after(function() {
+          client.jsonApiCall.restore();
+        });
+        
+        before(function(done) {
+          var directory = factory(idmap, client);
+          directory.list({ id: '1', username: 'johndoe' }, function(_err, _credentials) {
+            if (_err) { return done(_err); }
+            credentials = _credentials;
+            done();
+          });
+        });
+      
+        it('should map user identifier', function() {
+          expect(idmap).to.have.been.calledOnce;
+          var call = idmap.getCall(0);
+          expect(call.args[0]).to.deep.equal({
+            id: '1',
+            username: 'johndoe'
+          });
+        });
+        
+        it('should request authentication factors from Auth API', function() {
+          expect(client.jsonApiCall).to.have.been.calledOnce;
+          var call = client.jsonApiCall.getCall(0);
+          expect(call.args[0]).to.equal('POST');
+          expect(call.args[1]).to.equal('/auth/v2/preauth');
+          expect(call.args[2]).to.deep.equal({
+            username: 'johndoe'
+          });
+        });
+        
+        it('should not yield authenticators', function() {
+          expect(credentials).to.be.undefined;
+        });
+        
+      }); // user that needs to enroll
+      
     }); // #list
     
   }); // Directory
